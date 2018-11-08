6778bc8e0fb21f15133b58d6958ea33d65481d0b
Lorenzo Korn <lkorn.9520@gmail.com>
2018-11-08 15:38:54 +0100

    added CORS filter to the rest service

--- src/main/java/com/aquadis/rest/resources/UserResource.java
+++ src/main/java/com/aquadis/rest/resources/UserResource.java
@@ -34,6 +34,38 @@ public class UserResource {
         return service.getAllUsers();
     }
 
+    // TODO: FIX
+    @GET
+    @Path("/?username={username}&password={password}")
+    public Response getUser(@PathParam("username") String username, @PathParam("password") String password) {
+        List<User> users = service.getAllUsers();
+
+        for (int i = 0; i < users.size(); i++) {
+            if (users.get(i).getUserName().equals(username) && users.get(i).getPassword().equals(password)) {
+                return Response.ok(users.get(i).getId()).entity(users.get(i)).build();
+            }
+        }
+
+        return Response.status(Response.Status.FORBIDDEN).entity(new ClientError(username + " has no access")).build();
+    }
+
+    // TODO: FIX
+    @POST
+    @Path("/authenticate")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    public Response authenticateUser(@FormParam("email") String email, @FormParam("password") String password) {
+        List<User> users = service.getAllUsers();
+
+        for (int i = 0; i < users.size(); i++) {
+            if (users.get(i).getEmail().equals(email) && users.get(i).getPassword().equals(password)) {
+                return Response.ok(users.get(i).getId()).build();
+            }
+        }
+
+        return Response.status(Response.Status.FORBIDDEN).build();
+    }
+
     /**
      * Returns a specific user object based on its ID.
      * at: http://localhost:8080/aquadis/rest/users/{userID}
@@ -59,7 +91,7 @@ public class UserResource {
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public User addUser(User user){
+    public User addUser(User user) {
         return service.addUser(user);
     }
 
@@ -70,7 +102,7 @@ public class UserResource {
      * @return a new group resource
      */
     @Path("/{userID}/groups")
-    public GroupResource getGroupResource(){
+    public GroupResource getGroupResource() {
         return new GroupResource();
     }
 }
