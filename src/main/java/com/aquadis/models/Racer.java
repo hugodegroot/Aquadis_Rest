5428c200c8370389659aaf3c0251f5e219d8f68c
Lorenzo Korn <lkorn.9520@gmail.com>
2018-11-14 15:27:14 +0100

    added Racer entity

--- src/main/java/com/aquadis/models/Racer.java
+++ src/main/java/com/aquadis/models/Racer.java
@@ -1,4 +1,58 @@
 package com.aquadis.models;
 
+import org.hibernate.annotations.JoinColumnOrFormula;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Racer")
 public class Racer {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "firstname")
+    private String firstName;
+
+    @Column(name = "lastname")
+    private String lastName;
+
+    @Column(name = "salary")
+    private int salary;
+
+    public Racer(){
+
+    }
+
+    public Racer(String firstName, String LastName, int salary){
+        setFirstName(firstName);
+        setLastName(lastName);
+        setSalary(salary);
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    private void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    private void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    private void setSalary(int salary) {
+        this.salary = salary;
+    }
 }
