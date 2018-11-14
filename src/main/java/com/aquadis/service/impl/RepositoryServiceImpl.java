251bf167eec69afc78e8ad59d85a0110f6e42c3a
Lorenzo Korn <lkorn.9520@gmail.com>
2018-11-14 15:25:16 +0100

    implemented new methods

--- src/main/java/com/aquadis/service/impl/RepositoryServiceImpl.java
+++ src/main/java/com/aquadis/service/impl/RepositoryServiceImpl.java
@@ -1,7 +1,9 @@
 package com.aquadis.service.impl;
 
 import com.aquadis.models.Group;
+import com.aquadis.models.Racer;
 import com.aquadis.models.User;
+import com.aquadis.models.UserGroup;
 import com.aquadis.service.RepositoryService;
 
 import javax.persistence.EntityManager;
@@ -67,28 +69,47 @@ public class RepositoryServiceImpl implements RepositoryService {
 
     @Override
     public User addUser(User user) {
-        EntityManager entityManager = getEntityManager();
-
-        // Adds a user to the database
-        entityManager.getTransaction().begin();
-        entityManager.persist(user);
-        entityManager.getTransaction().commit();
-
-        entityManager.close();
-
-        return user;
+        return (User) addEntity(user);
     }
 
     @Override
-    public List<Group> getGroupsFromUser(User user) {
+    public List<UserGroup> getAllUserGroupsFromUser(int userID) {
         EntityManager entityManager = getEntityManager();
 
-        // TODO: usergroups link with group class?
-        // TODO: This correct?
-        Query query = entityManager.createQuery("SELECT u.groups FROM User u WHERE u.id = :userID");
-        query.setParameter("userID", user.getId());
+        Query query = entityManager.createQuery("SELECT u.groups FROM User u INNER JOIN u.groups WHERE u.id = :userID");
+        query.setParameter("userID", userID);
 
-        List<Group> groups = query.getResultList();
+        List<UserGroup> userGroups = query.getResultList();
+
+        entityManager.close();
+
+        return userGroups;
+    }
+
+    @Override
+    public List<UserGroup> getAllUserGroupsFromGroup(int groupID) {
+        EntityManager entityManager = getEntityManager();
+
+        Query query = entityManager.createQuery("SELECT g.users FROM Group g INNER JOIN g.users WHERE g.id = :groupID");
+        query.setParameter("groupID", groupID);
+
+        List<UserGroup> userGroups = query.getResultList();
+
+        entityManager.close();
+
+        return userGroups;
+    }
+
+    @Override
+    public UserGroup addUserGroup(UserGroup userGroup) {
+        return (UserGroup) addEntity(userGroup);
+    }
+
+    @Override
+    public List<Group> getAllGroups() {
+        EntityManager entityManager = getEntityManager();
+
+        List<Group> groups = entityManager.createQuery("SELECT g from Group g").getResultList();
 
         entityManager.close();
 
@@ -96,36 +117,46 @@ public class RepositoryServiceImpl implements RepositoryService {
     }
 
     @Override
-    public Group getGroupFromId(int userID, int groupID) {
+    public Group getGroupFromId(int groupID) {
         EntityManager entityManager = getEntityManager();
 
-        // TODO: Is this correct?
-        Query query = entityManager.createQuery("SELECT u.groups FROM User u WHERE Group.id = :groupID");
-        query.setParameter("groupID", groupID);
-
-        Group group = (Group) query.getSingleResult();
-
-        return group;
-    }
-
-    // TODO: how do add a group to a specific user?
-    @Override
-    public Group addGroup(int userID, Group group) {
-        User user = getUserFromId(userID);
-
-        EntityManager entityManager = getEntityManager();
-
-        entityManager.getTransaction().begin();
-        entityManager.persist(group);
-        entityManager.getTransaction().commit();
+        Group group = entityManager.find(Group.class, groupID);
 
         entityManager.close();
 
         return group;
     }
 
+    @Override
+    public Group addGroup(Group group) {
+        return (Group) addEntity(group);
+    }
+
+    @Override
+    public Racer addRacer(Racer racer) {
+        return (Racer) addEntity(racer);
+    }
+
+    /**
+     * Adds a object to the database. In our case the object will be a user or a group
+     *
+     * @param object User or Group
+     * @return the object
+     */
+    private Object addEntity(Object object) {
+        EntityManager entityManager = getEntityManager();
+
+        entityManager.getTransaction().begin();
+        entityManager.merge(object);
+        entityManager.getTransaction().commit();
+
+        entityManager.close();
+
+        return object;
+    }
 
     private void loadExamples() {
+        // Adds four users to the database
         User luuk = new User("luuk123@hotmail.nl", "luuk", "Luuk", "Goedhart", "luuk123", 1);
         addUser(luuk);
         User lorenzo = new User("lkorn.9520@gmail.com", "ALLIGAT0R_BL00D", "Lorenzo", "Korn", "lorenzo123", 1);
@@ -135,15 +166,77 @@ public class RepositoryServiceImpl implements RepositoryService {
         User hugo = new User("hugo123@outlook.nl", "hugo1997", "Hugo", "de Groot", "hugo123", 0);
         addUser(hugo);
 
-//        Group group1 = new Group("ALLIGAT0R_BL00D's group", lorenzo);
-//        group1.addUser(luuk);
-//        group1.addUser(janWillem);
-//
-//        Group group2 = new Group("luuk's group", luuk);
-//        group2.addUser(janWillem);
-//
-//        Group group3 = new Group("jw's group", janWillem);
-//        group3.addUser(lorenzo);
-//        group3.addUser(luuk);
+        // Adds twenty racers to the database
+        Racer hamilton = new Racer("", "Hamilton", 7000000);
+        addRacer(hamilton);
+        Racer bottas = new Racer("", "bottas", 6000000);
+        addRacer(bottas);
+        Racer ricciardo = new Racer("", "ricciardo", 6000000);
+        addRacer(ricciardo);
+        Racer verstappen = new Racer("", "verstappen", 6000000);
+        addRacer(verstappen);
+        Racer vettel = new Racer("", "vettel", 7000000);
+        addRacer(vettel);
+        Racer raikkonen = new Racer("", "raikkonen", 6000000);
+        addRacer(raikkonen);
+        Racer perez = new Racer("", "perez", 3000000);
+        addRacer(perez);
+        Racer ocon = new Racer("", "ocon", 3000000);
+        addRacer(ocon);
+        Racer huikenberg = new Racer("", "huikenberg", 4000000);
+        addRacer(huikenberg);
+        Racer sainz = new Racer("", "sainz", 3000000);
+        addRacer(sainz);
+        Racer vandoorne = new Racer("", "vandoorne", 3000000);
+        addRacer(vandoorne);
+        Racer alonzo = new Racer("", "alonzo", 4000000);
+        addRacer(alonzo);
+        Racer stroll = new Racer("", "stroll", 2000000);
+        addRacer(stroll);
+        Racer sirotkin = new Racer("", "Sirotkin", 1000000);
+        addRacer(sirotkin);
+        Racer gasly = new Racer("", "Gasly", 2000000);
+        addRacer(gasly);
+        Racer hartley = new Racer("", "Hartley", 2000000);
+        addRacer(hartley);
+        Racer grosjean = new Racer("", "Grosjean", 2000000);
+        addRacer(grosjean);
+        Racer magnussen = new Racer("", "Magnussen", 3000000);
+        addRacer(magnussen);
+        Racer ericsson = new Racer("", "Ericsson", 1000000);
+        addRacer(ericsson);
+        Racer leclerc = new Racer("", "Leclerc", 1000000);
+        addRacer(leclerc);
+
+        // Adds three groups to the database
+        Group group1 = new Group("ALLIGAT0R_BL00Ds group");
+        addGroup(group1);
+        Group group2 = new Group("School");
+        addGroup(group2);
+        Group group3 = new Group("Work");
+        addGroup(group3);
+
+        // TODO: fix this
+        // Adds usergroups
+//        UserGroup usergroup1 = new UserGroup(13, "admin", lorenzo, group1);
+//        addUserGroup(usergroup1);
+//        UserGroup usergroup2 = new UserGroup(5, "member", luuk, group1);
+//        addUserGroup(usergroup2);
+//        UserGroup usergroup3 = new UserGroup(8, "member", janWillem, group1);
+//        addUserGroup(usergroup3);
+//        UserGroup usergroup4 = new UserGroup(19, "admin", luuk, group2);
+//        addUserGroup(usergroup4);
+//        UserGroup usergroup5 = new UserGroup(11, "member", lorenzo, group2);
+//        addUserGroup(usergroup5);
+//        UserGroup usergroup6 = new UserGroup(11, "member", hugo, group2);
+//        addUserGroup(usergroup6);
+//        UserGroup usergroup7 = new UserGroup(11, "admin", janWillem, group3);
+//        addUserGroup(usergroup7);
+//        UserGroup usergroup8 = new UserGroup(11, "member", lorenzo, group3);
+//        addUserGroup(usergroup8);
+//        UserGroup usergroup9 = new UserGroup(11, "member", luuk, group3);
+//        addUserGroup(usergroup9);
+//        UserGroup usergroup10 = new UserGroup(11, "member", hugo, group3);
+//        addUserGroup(usergroup10);
     }
 }
