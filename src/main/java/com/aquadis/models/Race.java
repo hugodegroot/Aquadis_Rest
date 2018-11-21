39ede748a4d121b2cb14a54eb9722d00198987d8
Lorenzo Korn <lkorn.9520@gmail.com>
2018-11-21 11:27:02 +0100

    Added race class which racers can participate in

--- src/main/java/com/aquadis/models/Race.java
+++ src/main/java/com/aquadis/models/Race.java
@@ -1,7 +1,7 @@
 package com.aquadis.models;
 
 import javax.persistence.*;
-import java.util.Date;
+import java.util.List;
 
 @Entity
 @Table(name = "Race")
@@ -13,11 +13,67 @@ public class Race {
     private int id;
 
     @Column(name = "name")
-    private String Name;
+    private String name;
 
-    @Column(name = "begin_date")
-    private Date beginDate;
+    @Column(name = "month")
+    private String month;
 
-    @Column(name = "end_date")
-    private Date endDate;
+    @Column(name = "start_day")
+    private int startDay;
+
+    @Column(name = "end_day")
+    private int endDay;
+
+    @OneToMany(mappedBy = "race", fetch = FetchType.EAGER)
+    private List<RacePosition> positions;
+
+    public Race() {
+    }
+
+    public Race(String name, String month, int start, int end) {
+        setName(name);
+        setMonth(month);
+        setStartDay(start);
+        setEndDay(end);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMonth() {
+        return month;
+    }
+
+    private void setMonth(String month) {
+        this.month = month;
+    }
+
+    public int getStartDay() {
+        return startDay;
+    }
+
+    private void setStartDay(int startDay) {
+        this.startDay = startDay;
+    }
+
+    public int getEndDay() {
+        return endDay;
+    }
+
+    private void setEndDay(int endDay) {
+        this.endDay = endDay;
+    }
+
+    public List<RacePosition> getPositions() {
+        return positions;
+    }
+
+    public void setPositions(List<RacePosition> positions) {
+        this.positions = positions;
+    }
 }
